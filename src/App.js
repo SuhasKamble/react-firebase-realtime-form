f750f40229229df52fce23cd934679a315ed0a08
Suhas <kamblesuhas12@gmail.com>
2021-08-16 11:28:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState({
+    fname:"",
+    lname:"",
+    email:"",
+    phone:"",
+    message:""
+  })
+
+  let name, value;
+
+  const getUserData=(e)=>{
+    name = e.target.name;
+    value = e.target.value;
+    setUsers({...users,[name]:value});
+  }
+
+  const submitForm = async(e) =>{
+    e.preventDefault()
+    const {fname, lname, email, phone, message} = users;
+
+    if(!fname || !lname || !email || !phone || !message){
+      toast.error('🦄 Please fill the form correctly!', {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        });
+    }
+    else{
+      const res = await fetch("https://fir-react-realtime-default-rtdb.firebaseio.com/reactform.json",{
+        method:"POST",
+        headers:{
+          "Content-Type":"application/json",
+        },
+        body:JSON.stringify({
+          fname,
+          lname, 
+          email,
+          phone,
+          message
+        })
+      })
+  
+      if(res){
+        toast.success('🦄 Form successfully submiited!', {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          });
+      }
+    }
+
+
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="big-container">
+        <div class="container">
+            <h2>Contact Form</h2>
+            <div class="input-container">
+                <input type="text" name="fname" value={users.fname} onChange={getUserData} placeholder="First Name" autocomplete="off"/>
+                <input type="text" name="lname" value={users.lname} onChange={getUserData} placeholder="Last Name" autocomplete="off"/>
+            </div>
+            <div class="input-container">
+                <input type="text" name="email" value={users.email} onChange={getUserData} placeholder="Email" autocomplete="off"/>
+                <input type="number" name="phone" value={users.phone} onChange={getUserData} placeholder="Phone" autocomplete="off"/>
+            </div>
+            <textarea name="message" value={users.message} onChange={getUserData}  placeholder="Enter message..."></textarea>
+            <button class="btn" onClick={submitForm} type="submit">Submit</button>
+        </div>
+        <ToastContainer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
